c61d0dac24db25986cba816c4d802b5c3fe99a17
blaze <prabhatkumarverma42@gmail.com>
2024-07-11 07:17:30 +0530

    Solution for exercise 1.7 and 1.8

--- part1/courseinfo/src/components/Feedback/Statistics/Statistics.jsx
+++ part1/courseinfo/src/components/Feedback/Statistics/Statistics.jsx
@@ -2,12 +2,19 @@ import Header from "../Header/Header";
 
 const Statistics = ({ good, bad, neutral }) => {
   const heading = "Statistics";
+  const total = good + bad + neutral;
+  const average = (good - bad) / total;
+  const positivePercent = (good / total) * 100;
+
   return (
     <>
       <Header text={heading} />
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercent} %</p>
     </>
   );
 };
