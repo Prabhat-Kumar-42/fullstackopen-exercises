ba92f01ce727713ae913dc71cad4c27f2fb62d40
blaze <prabhatkumarverma42@gmail.com>
2024-07-13 08:10:09 +0530

    Solution for exercise 2.13

---
+++ part2/phonebook/src/services/persons.services.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/persons";
+
+const getAllUser = () => {
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
+
+const getUser = (user) => {
+  const id = user.id;
+  const request = axios.get(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const createUser = (user) => {
+  const request = axios.post(baseUrl, user);
+  return request.then((response) => response.data);
+};
+
+const deleteUser = (user) => {
+  const id = user.id;
+  const request = axios.delete(`${baseUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+const putUser = (user) => {
+  const id = user.id;
+  delete user.id;
+  const request = axios.put(`${baseUrl}/${id}`, user);
+  return request.then((response) => response.data);
+};
+
+export default {
+  getAllUser,
+  getUser,
+  createUser,
+  deleteUser,
+  putUser,
+};
