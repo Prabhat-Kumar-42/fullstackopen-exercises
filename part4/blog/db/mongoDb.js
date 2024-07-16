38da2113a1ade264d115a6dc6e5c374d7f10274e
blaze <prabhatkumarverma42@gmail.com>
2024-07-16 18:45:39 +0530

    added module to connect to db

---
+++ part4/blog/db/mongoDb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { MONGO_URL } = require("../utils/config");
+const { info } = require("../utils/logger");
+
+const mongoUrl = MONGO_URL;
+
+const mongoConnect = async () => {
+  await mongoose.connect(mongoUrl);
+  info("Connected To Database Successfully");
+};
+
+module.exports = mongoConnect;
