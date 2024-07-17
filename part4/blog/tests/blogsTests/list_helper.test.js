8685447697221456a77a4738c628d2184a4c1dd4
blaze <prabhatkumarverma42@gmail.com>
2024-07-17 17:16:17 +0530

    moved to /tests/blogsTests/utilityTest/tests

--- part4/blog/tests/blogsTests/list_helper.test.js
+++
@@ -1,8 +0,0 @@
-const { test } = require("node:test");
-const assert = require("node:assert");
-const { dummy } = require("../../utils/blogsUtils/list_helper.js");
-
-test("dummy test 1", () => {
-  const blogs = {};
-  assert.strictEqual(dummy(blogs), 1);
-});
