3549a5df7f5d087ad817b034cdb5be84987f4036
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-20 20:48:48 +0530

    added put and delete route for blogs

--- part4/blog/controller/api/blogs.controller.js
+++ part4/blog/controller/api/blogs.controller.js
@@ -1,15 +1,8 @@
 const Blog = require("../../models/blog.model.js");
 
-const handleGetAllBlogs = (request, response) => {
-  Blog.find({}).then((blogs) => {
-    return response.json(blogs);
-  });
-};
-
-const handleGetBlog = async (request, response) => {
-  const id = request.params.id;
-  const blog = await Blog.findById(id);
-  return response.status(200).json(blog);
+const handleGetAllBlogs = async (request, response) => {
+  const blogs = await Blog.find({});
+  return response.json(blogs);
 };
 
 const handleCreateBlog = async (request, response) => {
@@ -21,8 +14,31 @@ const handleCreateBlog = async (request, response) => {
   return response.status(201).json(blog);
 };
 
+const handleGetBlog = async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findById(id);
+  if (!blog) response.status(404).end();
+  return response.status(200).json(blog);
+};
+
+const handleDeleteBlog = async (request, response) => {
+  const id = request.params.id;
+  const blog = await Blog.findByIdAndDelete(id);
+  return response.status(200).json(blog);
+};
+
+const handleUpdateBlog = async (request, response) => {
+  const id = request.params.id;
+  if (!id) response.status(400).end();
+  const updateData = response.body();
+  const blog = await Blog.findByIdAndUpdate(id, updateData);
+  return response.status(200).json(blog);
+};
+
 module.exports = {
   handleGetAllBlogs,
   handleCreateBlog,
   handleGetBlog,
+  handleDeleteBlog,
+  handleUpdateBlog,
 };
--- part4/blog/router/api/blogs.router.js
+++ part4/blog/router/api/blogs.router.js
@@ -4,8 +4,16 @@ const {
   handleGetAllBlogs,
   handleCreateBlog,
   handleGetBlog,
+  handleDeleteBlog,
+  handleUpdateBlog,
 } = require("../../controller/api/blogs.controller");
 
 blogRouter.route("/").get(handleGetAllBlogs).post(handleCreateBlog);
-blogRouter.route("/:id").get(handleGetBlog);
+
+blogRouter
+  .route("/:id")
+  .get(handleGetBlog)
+  .delete(handleDeleteBlog)
+  .put(handleUpdateBlog);
+
 module.exports = blogRouter;
