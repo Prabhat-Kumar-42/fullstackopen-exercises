f189b64c02799e7d398359ea64464b699a221970
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-20 15:51:39 +0530

    added solution to exercise 4.8-4.9
    - added database cleanup and population after every test
    - added test for exercise 4.8
    - added test for exercise 4.9

--- part4/blog/tests/blogsTests/apiTest/apiUtilityGroup.test.js
+++ part4/blog/tests/blogsTests/apiTest/apiUtilityGroup.test.js
@@ -1,9 +1,31 @@
-const { describe, it, before, after } = require("node:test");
-const { setUpTestServer, tearDownTestServer } = require("../../../utils/mongoMemoryServer");
+const {
+  describe,
+  test,
+  before,
+  after,
+  beforeEach,
+  afterEach,
+} = require("node:test");
 
-describe("Blogs API Group Test", () => {
+const {
+  setUpTestServer,
+  tearDownTestServer,
+} = require("../../../utils/mongoMemoryServer");
+
+const assert = require("node:assert");
+const supertest = require("supertest");
+const app = require("../../../app");
+const api = supertest(app);
+const baseUrl = "/api/blogs/";
+
+const Blog = require("../../../models/blog.model");
+const { blogsSampleData } = require("../blogSampleData");
+
+describe("Blogs API Group Test", async () => {
   let mongoServer;
   let serverConnection;
+  let blogList = [];
+
   before(async () => {
     const serverInfo = await setUpTestServer();
     mongoServer = serverInfo.mongoServer;
@@ -13,5 +35,35 @@ describe("Blogs API Group Test", () => {
   after(async () => {
     await tearDownTestServer(mongoServer, serverConnection);
   });
-});
 
+  beforeEach(async () => {
+    for (let blog of blogsSampleData) {
+      const createdBlog = await Blog.create(blog);
+      blogList.push(createdBlog);
+    }
+  });
+
+  afterEach(async () => {
+    await Blog.deleteMany({});
+    blogList = [];
+  });
+
+  test("fetch all data from server", async () => {
+    const response = await api
+      .get(baseUrl)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsLength = response.body.length;
+    assert.strictEqual(blogsLength, blogList.length);
+  });
+
+  test("response id field is same is db _id field", async () => {
+    const url = baseUrl + blogList[0]._id;
+    const response = await api
+      .get(url)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+    assert.deepStrictEqual(blogList[0]._id.toString(), response.body.id);
+  });
+});
