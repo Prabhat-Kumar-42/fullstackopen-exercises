044f8b8b80146976d366da1e2a92b9b1a059c34b
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-20 15:53:47 +0530

    added transform to (rename _id to id) and (remove __v)

--- part4/blog/models/blog.model.js
+++ part4/blog/models/blog.model.js
@@ -7,6 +7,14 @@ const blogSchema = new mongoose.Schema({
   likes: Number,
 });
 
+blogSchema.set("toJSON", {
+  transform: (document, returnObject) => {
+    returnObject.id = returnObject._id.toString();
+    delete returnObject._id;
+    delete returnObject.__v;
+  },
+});
+
 const Blog = mongoose.model("Blog", blogSchema);
 
 module.exports = Blog;
