f7ea84677adf04108184a5150b4b5e1f248b8b6c
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-21 12:30:55 +0530

    added error handler middewares

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+require("express-async-errors");
 const cors = require("cors");
 const blogRouter = require("./router/api/blogs.router");
+const {
+  unknownEndpoint,
+  mongoError,
+  errorHandler,
+} = require("./middlewares/errorHandlers.middleware");
 
 const app = express();
 
@@ -10,5 +16,11 @@ app.use(express.json());
 
 // Routes
 app.use("/api/blogs", blogRouter);
+
+//Error Handlers
+
+app.use(unknownEndpoint);
+app.use(mongoError);
+app.use(errorHandler);
 
 module.exports = app;
