77581f176a0e3c6a23b38214262bba6c41c18014
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-22 00:50:27 +0530

    added user mock data

---
+++ part4/blog/tests/testUtilities/mockData/userSampleData.js
@@ -0,0 +1,11 @@
+const userSampleData = [
+  { username: "nkelner0", name: "Niki", password: "Kelner" },
+  { username: "mbrogini1", name: "Moss", password: "Brogini" },
+  { username: "bdanilewicz2", name: "Bob", password: "Danilewicz" },
+  { username: "jheadingham3", name: "Johannes", password: "Headingham" },
+  { username: "jsmidmor4", name: "Jacquelin", password: "Smidmor" },
+];
+
+module.exports = {
+  userSampleData,
+};
