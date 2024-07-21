561941ffeeca490d8622ccd2981bd28b82df158f
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-22 00:47:10 +0530

    renamed USER -> User, destructured few elements

--- part4/blog/controller/api/users.controller.js
+++ part4/blog/controller/api/users.controller.js
@@ -1,16 +1,14 @@
-const { USER } = require("../../models/user.model");
+const User = require("../../models/user.model");
 const { passwordValidation } = require("../../utils/passwordValidation");
 const throwError = require("../../utils/throwError");
 
 const handleSignUp = async (req, res) => {
   if (!req.body) throwError("400", "Bad Request");
-  const username = req.body.username;
-  const password = req.body.password;
-  const name = req.body.name;
+  const { username, password, name } = req.body;
   const validation = passwordValidation(password);
   if (!validation.status) throwError(400, validation.message);
   // password hash is handeled by model schema, look at schema code for more details
-  const user = await USER.create({
+  const user = await User.create({
     username,
     name,
     hashedPassword: password,
@@ -20,10 +18,9 @@ const handleSignUp = async (req, res) => {
 
 const handleLogin = async (req, res) => {
   if (!req.body) throwError("400", "Bad Request");
-  const username = req.body.username;
-  const password = req.body.password;
+  const { username, password } = req.body;
   // incorrect password is handeled by model schema, look at schema code for more details
-  const user = USER.matchPassword(username, password);
+  const user = await User.matchPassword(username, password);
   return res.status(200).json({ message: "login success", user });
 };
 
