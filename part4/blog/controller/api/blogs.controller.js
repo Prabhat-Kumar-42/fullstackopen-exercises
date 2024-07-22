daabe4aafef8f581525e53e54f73521a88c9cbeb
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-22 16:07:18 +0530

    Major Changes: Read Commit Description for details. tl;dr - added authorizaion to specific routes  and population of reference fields
    - Added Authorization to post, put and delete routes of the blog api,
      i.e, only authozired users, i.e, blog's creater/ author are the only
      ones allowed to modify and delete blogs and only authenticated user
      can create new blog posts.
    
    - Added data populate to reference fields.

--- part4/blog/controller/api/blogs.controller.js
+++ part4/blog/controller/api/blogs.controller.js
@@ -3,7 +3,7 @@ const User = require("../../models/user.model.js");
 const throwError = require("../../utils/throwError.js");
 
 const handleGetAllBlogs = async (request, response) => {
-  const blogs = await Blog.find({}).populate("user");
+  const blogs = await Blog.find({}).populate("author");
   return response.json(blogs);
 };
 
@@ -12,38 +12,52 @@ const handleCreateBlog = async (request, response) => {
   if (!blogData.title || !blogData.url) {
     throwError(400, "title and url are required fields");
   }
-  const author = await User.findOne({});
-  blogData.user = author.id;
+  const authorId = request.user.id;
+  const author = await User.findById(authorId);
+  if (!author) throwError(404, "user not found");
+  blogData.author = author.id;
   let blog = await Blog.create(blogData);
   author.blogs.push(blog.id);
-  author.save();
-  blog = await Blog.populate(blog, { path: "user" });
+  await author.save();
+  blog = await Blog.populate(blog, { path: "author" });
   return response.status(201).json(blog);
 };
 
 const handleGetBlog = async (request, response) => {
   const id = request.params.id;
-  const blog = await Blog.findById(id).populate("user");
+  const blog = await Blog.findById(id).populate("author");
   if (!blog) throwError(404, "Not Found");
   return response.status(200).json(blog);
 };
 
 const handleDeleteBlog = async (request, response) => {
   const id = request.params.id;
-  const blog = await Blog.findByIdAndDelete(id).populate("user");
+  if (!id) throwError(404, "Not Found");
+  const user = request.user;
+  const author = await User.findById(user.id);
+  if (!author) throwError(404, "use not found");
+  const blog = await Blog.findById(id).populate("author");
   if (!blog) return response.status(204).end();
-  return response.status(200).json(blog);
+  if (blog.author.id.toString() !== user.id) throwError(403, "Forbidden");
+  await Blog.deleteOne({ _id: blog.id }).populate("author");
+  author.blogs = author.blogs.filter(
+    (userBlog) => userBlog.id.toString() !== blog.id.toString(),
+  );
+  await author.save();
+  return response.status(200).json({ message: "blog deleted successfully" });
 };
 
 const handleUpdateBlog = async (request, response) => {
   const id = request.params.id;
   if (!id) throwError(404, "Not Found");
+  const user = request.user;
   const updateData = request.body;
-  const blog = await Blog.findByIdAndUpdate(id, updateData, {
-    new: true,
-    runValidators: true,
-  }).populate("user");
+  const blog = await Blog.findById(id);
   if (!blog) throwError(404, "Not Found");
+  if (blog.author.toString() !== user.id) throwError(403, "Forbidden");
+  blog.set(updateData);
+  await blog.save();
+  await blog.populate("author");
   return response.status(200).json(blog);
 };
 
