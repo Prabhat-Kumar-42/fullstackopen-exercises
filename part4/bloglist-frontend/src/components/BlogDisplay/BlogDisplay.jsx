bee39d16d4d49eee0608673d8df7a4fcc3c8aa14
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-26 22:09:05 +0530

    seperated BlogDisplay as a seperate component

---
+++ part4/bloglist-frontend/src/components/BlogDisplay/BlogDisplay.jsx
@@ -0,0 +1,28 @@
+import Header from "../Header/Header";
+import Button from "../Button/Button";
+import BlogForm from "../BlogForm/BlogForm";
+
+const BlogDisplay = ({ blogs, user, setBlogs, handleLogout }) => {
+  return (
+    <div>
+      <Header heading={"blogs"} type={1} />
+      <BlogForm blogs={blogs} setBlogs={setBlogs} />
+      <p>{user.name} is logged in !!</p>
+      <Button
+        title={"logout"}
+        buttonType={"submit"}
+        onEvent={"onClick"}
+        eventHandler={handleLogout}
+      />
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          <p>
+            {blog.title} {blog.author.name}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogDisplay;
