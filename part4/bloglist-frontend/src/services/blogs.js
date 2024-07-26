cad0624f614bf057c9cef41e2d4e873c223c4fb2
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-26 22:11:03 +0530

    added post blog service

--- part4/bloglist-frontend/src/services/blogs.js
+++ part4/bloglist-frontend/src/services/blogs.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 const baseUrl = "/api/blogs";
 
-let authToken = null;
+let authToken = "";
 
-const setToken = (newToken) => (authToken = newToken);
+const setToken = (newToken) => {
+  authToken = `Bearer ${newToken}`;
+};
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
-export default { getAll, setToken };
+const postBlog = async (payload) => {
+  const response = await axios.post(baseUrl, payload, {
+    headers: { Authorization: authToken },
+  });
+  return response.data;
+};
 
+export default { getAll, setToken, postBlog };
