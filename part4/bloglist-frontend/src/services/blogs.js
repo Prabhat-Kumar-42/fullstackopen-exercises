c5e2e94ca34ce935331fa1752e8016f2572a0724
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-26 18:05:15 +0530

    added set token for auth headers

--- part4/bloglist-frontend/src/services/blogs.js
+++ part4/bloglist-frontend/src/services/blogs.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 const baseUrl = "/api/blogs";
 
+let authToken = null;
+
+const setToken = (newToken) => (authToken = newToken);
+
 const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
-export default { getAll };
+export default { getAll, setToken };
 
