88b026722f51ce7dfbf0ac87582bdf94234c99e3
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-27 23:03:44 +0530

    updated blog update logic

--- part4/blog/controller/api/blogs.controller.js
+++ part4/blog/controller/api/blogs.controller.js
@@ -51,11 +51,15 @@ const handleUpdateBlog = async (request, response) => {
   const id = request.params.id;
   if (!id) throwError(404, "Not Found");
   const user = request.user;
-  const updateData = request.body;
+  const { title, url, likes } = request.body;
+  const updates = {};
+  if (likes) updates.likes = likes;
+  if (url) updates.url = url;
+  if (title) updates.title = title;
   const blog = await Blog.findById(id);
   if (!blog) throwError(404, "Not Found");
   if (blog.author.toString() !== user.id) throwError(403, "Forbidden");
-  blog.set(updateData);
+  blog.set(updates);
   await blog.save();
   await blog.populate("author");
   return response.status(200).json(blog);
