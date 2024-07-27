98ce2d5a1b018a0c105607cb36395f985230e16b
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-27 23:06:30 +0530

    added logic to like the blog

--- part4/bloglist-frontend/src/components/BlogDisplay/DisplaySpecificBlog/DisplaySpecificBlog.jsx
+++ part4/bloglist-frontend/src/components/BlogDisplay/DisplaySpecificBlog/DisplaySpecificBlog.jsx
@@ -2,8 +2,15 @@ import "./style.css";
 
 import { useRef } from "react";
 import Toggleable from "../../Toggleable/Toggleable";
+import blogServices from "../../../services/blogs";
+import Button from "../../Button/Button";
 
-const DisplaySpecificBlog = ({ blog }) => {
+const DisplaySpecificBlog = ({
+  blog,
+  handleSuccessMessage,
+  handleErrorMessage,
+  handleUpdates,
+}) => {
   const notice = ` when i started the exericese from previous parts, 
     I had set user/poster as the author of the blog.
     So likes and url are the only field left to display, 
@@ -17,6 +24,21 @@ const DisplaySpecificBlog = ({ blog }) => {
   const toDisplayAuthorNotice = "show author notice";
   const toHideAuthorNotice = "close author notice";
 
+  const updateLike = async () => {
+    const likes = blog.likes + 1;
+    const payload = { likes };
+    try {
+      await blogServices.updateBlog(blog, payload);
+      handleUpdates();
+      const successMessage = `you liked, ${blog.title}`;
+      handleSuccessMessage(successMessage, 3000);
+    } catch (error) {
+      console.log(error);
+      const newErrorMessage = error.response.data.error;
+      handleErrorMessage(newErrorMessage, 3000);
+    }
+  };
+
   return (
     <div className="blogStyle">
       <p>title: {blog.title}</p>
@@ -27,7 +49,15 @@ const DisplaySpecificBlog = ({ blog }) => {
         ref={blogRef}
       >
         <p>url: {blog.url}</p>
-        <p>ikes: {blog.likes}</p>
+        <p>
+          likes: {blog.likes}
+          <Button
+            buttonType={"submit"}
+            title={"like"}
+            onEvent={"onClick"}
+            eventHandler={updateLike}
+          />
+        </p>
         authorNotice:{" "}
         <Toggleable
           toDisplayTitle={toDisplayAuthorNotice}
