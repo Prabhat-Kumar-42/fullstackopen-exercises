44f9021e18e709593bc4f5d760949d3c2d6462b1
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-28 20:10:10 +0530

    added test for displaySpecificBlog component

---
+++ part5/bloglist-frontend/src/components/BlogDisplay/DisplaySpecificBlog/DisplaySpecificBlog.test.js
@@ -0,0 +1,44 @@
+import { render } from "@testing-library/react";
+import DisplaySpecificBlog from "./DisplaySpecificBlog";
+import { expect, test, vi } from "vitest";
+
+test("title", () => {
+  const mockHandleSuccessMessage = vi.fn();
+  const mockHandleErrorMessage = vi.fn();
+  const mockHandleUpdates = vi.fn();
+
+  const mockBlog = {
+    title: "test blog",
+    url: "https://testBlogUrl.com",
+    likes: 7,
+    author: {
+      name: "L",
+      id: "3181979",
+    },
+  };
+
+  const mockUser = {
+    name: "L",
+    id: "3181979",
+  };
+
+  const { container } = render(
+    <DisplaySpecificBlog
+      user={mockUser}
+      blog={mockBlog}
+      handleSuccessMessage={mockHandleSuccessMessage}
+      handleErrorMessage={mockHandleErrorMessage}
+      handleUpdates={mockHandleUpdates}
+    />,
+  );
+
+  const titleElement = container.querySelector(".title");
+  const authorElement = container.querySelector(".author");
+  const urlElement = container.querySelector(".url");
+  const likesElement = container.querySelector(".likes");
+
+  expect(titleElement).toBeDefined();
+  expect(authorElement).toBeDefined();
+  expect(urlElement).toBeNull();
+  expect(likesElement).toBeNull();
+});
