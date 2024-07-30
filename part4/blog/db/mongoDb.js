f35627428dc3129e8e9564bb43e38183c62d8dc2
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-30 18:54:25 +0530

    minor

--- part4/blog/db/mongoDb.js
+++ part4/blog/db/mongoDb.js
@@ -3,7 +3,6 @@ const { MONGO_URL } = require("../utils/config");
 const { info } = require("../utils/logger");
 
 const mongoUrl = MONGO_URL;
-
 const mongoConnect = async () => {
   await mongoose.connect(mongoUrl);
   info("Connected To Database Successfully");
