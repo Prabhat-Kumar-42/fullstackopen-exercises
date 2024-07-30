e7fb07b031b03e2a4eff307f9a44a45e20eaae96
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-30 20:20:23 +0530

    added login helper

--- part5/e2eTests-playwright/testHelpers/login.testHelper.js
+++ part5/e2eTests-playwright/testHelpers/login.testHelper.js
@@ -1,8 +1,13 @@
-const login = (page, username, password) => {};
+const login = async ({ page, username, password }) => {
+  await page.getByTestId("loginForm");
+  await page.getByTestId("loginFormUserNameField").fill(username);
+  await page.getByTestId("loginFormPasswordField").fill(password);
+  await page.getByTestId("loginFormSubmitButton").click();
+};
 
 const signup = async ({ page, username, name, password }) => {
   //await page.goto("http://localhost:5173/");
-  await page.getByTestId("singpForm");
+  await page.getByTestId("signupForm");
   await page.getByTestId("signupFormUserNameField").fill(username);
   await page.getByTestId("signupFormNameField").fill(name);
   await page.getByTestId("signupFormPasswordField").fill(password);
@@ -11,4 +16,5 @@ const signup = async ({ page, username, name, password }) => {
 
 module.exports = {
   signup,
+  login,
 };
