08b94bd11b39b20ce38ffe27554434585381f9d4
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-30 18:55:21 +0530

    added logic to get db url according to the environment

--- part4/blog/utils/config.js
+++ part4/blog/utils/config.js
@@ -1,8 +1,15 @@
 require("dotenv").config();
+
+const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3001;
-const MONGO_URL = process.env.MONGO_URL;
+
+const getMongoUrl = () =>
+  ENV === "test" ? process.env.MONGO_URL_TEST : process.env.MONGO_URL;
+
+const MONGO_URL = getMongoUrl();
 
 module.exports = {
+  ENV,
   PORT,
   MONGO_URL,
 };
