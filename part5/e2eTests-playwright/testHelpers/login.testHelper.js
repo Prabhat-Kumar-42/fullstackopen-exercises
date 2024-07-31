b3faff9e3afe00ca16a17663a8d5ff1ef1ddfd57
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-31 11:02:28 +0530

    added helper to signup and login simultaniously, updated createBlog

--- part5/e2eTests-playwright/testHelpers/login.testHelper.js
+++ part5/e2eTests-playwright/testHelpers/login.testHelper.js
@@ -1,3 +1,5 @@
+const { expect } = require("@playwright/test");
+
 const login = async ({ page, username, password }) => {
   await page.getByTestId("loginForm");
   await page.getByTestId("loginFormUserNameField").fill(username);
@@ -13,16 +15,39 @@ const signup = async ({ page, username, name, password }) => {
   await page.getByTestId("signupFormSubmitButton").click();
 };
 
+const signupAndLogin = async (userInfo) => {
+  const { page } = userInfo;
+  await page.getByTestId("swithToSignUpFormButton").click();
+  await signup(userInfo);
+
+  await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+  await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
+
+  await page.getByTestId("swithToLoginFormButton").click();
+
+  await login(userInfo);
+
+  await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+  await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
+};
+
 const createBlog = async ({ page, title, url }) => {
-  await page.getByTestId("blogForm");
   await page.getByTestId("showVisibilityButton").click();
+
+  await expect(page.getByTestId("blogForm")).toBeVisible();
   await page.getByTestId("blogFormTitleField").fill(title);
   await page.getByTestId("blogFormUrlField").fill(url);
+
   await page.getByTestId("postBlogButton").click();
+
+  await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+  await expect(page.getByTestId("blogList")).not.toBeEmpty();
+  await expect(page.getByTestId("blogForm")).toBeHidden();
 };
 
 module.exports = {
   signup,
   login,
+  signupAndLogin,
   createBlog,
 };
