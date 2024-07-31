f70cf46f50792118e080b49ed8074d2532c1aa88
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-31 10:30:30 +0530

    changed success message

--- part5/bloglist-frontend/src/components/LoginSignUp/LoginSignUp.jsx
+++ part5/bloglist-frontend/src/components/LoginSignUp/LoginSignUp.jsx
@@ -36,7 +36,7 @@ const LoginSignUp = ({ setUser, handleSuccessMessage, handleErrorMessage }) => {
       const user = responseData.user;
       localStorage.setItem("loggedUser", JSON.stringify(user));
       blogService.setToken(user.authToken);
-      handleSuccessMessage("successfully logged in", 3000);
+      handleSuccessMessage("login successfull", 3000);
       setUser(user);
       setUserName("");
       setPassword("");
