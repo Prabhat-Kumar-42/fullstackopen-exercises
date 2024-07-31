865844a554b7c864a7f411b5b8a610fe6ed4c389
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-07-31 10:31:13 +0530

    fixed all ambiguity and flakiness of the tests till now

--- part5/e2eTests-playwright/tests/blog.spec.js
+++ part5/e2eTests-playwright/tests/blog.spec.js
@@ -11,17 +11,13 @@ const getUserInfoFor = (action) => {
   if (action !== "signup") {
     delete payload.name;
   }
-
   return payload;
 };
 
-const getBlogPayload = () => {
-  const payload = {
-    title: "testBlog-1",
-    url: "http://www.testblog-1.com",
-  };
-  return payload;
-};
+const getBlogPayload = () => ({
+  title: "testBlog-1",
+  url: "http://www.testblog-1.com",
+});
 
 test.describe("blog app", () => {
   test.beforeEach(async ({ page }) => {
@@ -30,82 +26,109 @@ test.describe("blog app", () => {
   });
 
   test("Login Form is Shown", async ({ page }) => {
-    await page.getByTestId("loginForm").isVisible();
-    await page.getByTestId("signupForm").isHidden();
+    await expect(page.getByTestId("loginForm")).toBeVisible();
+    await expect(page.getByTestId("signupForm")).toBeHidden();
   });
 
-  test.describe("User SignUp Test", async () => {
-    test("successfull signup", async ({ page }) => {
+  test.describe("User SignUp Test", () => {
+    test("successful signup", async ({ page }) => {
       const signupInfo = getUserInfoFor("signup");
       await page.getByTestId("swithToSignUpFormButton").click();
       signupInfo.page = page;
       await signup(signupInfo);
-      await page.getByTestId("successMessageDisplay");
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
     });
 
-    test("unsuccessfull signup", async ({ page }) => {
+    test("unsuccessful signup", async ({ page }) => {
       const signupInfo = getUserInfoFor("signup");
       await page.getByTestId("swithToSignUpFormButton").click();
       signupInfo.page = page;
       await signup(signupInfo);
-      await page.getByTestId("successMessageDisplay");
-      await signup(signupInfo);
-      await page.getByTestId("errorMessageDisplay");
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
+
+      await signup(signupInfo); // Trigger an error
+      await expect(page.getByTestId("errorMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("errorMessageDisplay")).toBeHidden();
     });
   });
 
-  test.describe("User Login Test", async () => {
+  test.describe("User Login Test", () => {
     test.beforeEach(async ({ page }) => {
       const signupInfo = getUserInfoFor("signup");
       await page.getByTestId("swithToSignUpFormButton").click();
       signupInfo.page = page;
       await signup(signupInfo);
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
+
       await page.getByTestId("swithToLoginFormButton").click();
     });
-    test("successfull login test", async ({ page }) => {
+
+    test("successful login test", async ({ page }) => {
       const loginInfo = getUserInfoFor("login");
       loginInfo.page = page;
       await login(loginInfo);
-      await page.getByTestId("successMessageDisplay");
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
     });
 
-    test("unsuccessfull login test", async ({ page }) => {
+    test("unsuccessful login test", async ({ page }) => {
       const loginInfo = getUserInfoFor("login");
       loginInfo.page = page;
       loginInfo.password = "123";
       await login(loginInfo);
-      await page.getByTestId("errorMessageDisplay");
+
+      await expect(page.getByTestId("errorMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("errorMessageDisplay")).toBeHidden();
     });
   });
 
-  test.describe("When Logged In", async () => {
+  test.describe("When Logged In", () => {
     test.beforeEach(async ({ page }) => {
       const signupInfo = getUserInfoFor("signup");
       await page.getByTestId("swithToSignUpFormButton").click();
       signupInfo.page = page;
       await signup(signupInfo);
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
+
       await page.getByTestId("swithToLoginFormButton").click();
       const loginInfo = getUserInfoFor("login");
       loginInfo.page = page;
       await login(loginInfo);
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("successMessageDisplay")).toBeHidden();
     });
 
-    test("landed on blog page after successfull login", async ({ page }) => {
+    test("landed on blog page after successful login", async ({ page }) => {
       const userInfo = getUserInfoFor("signup");
-      await page.getByRole("heading", { name: "blogs" });
-      expect(page.getByText(`${userInfo.name} is logged in !!}`));
+      await expect(page.getByRole("heading", { name: "blogs" })).toBeVisible();
+      await expect(
+        page.getByText(`${userInfo.name} is logged in !!`),
+      ).toBeVisible();
     });
 
     test("a new blog can be created", async ({ page }) => {
       const { title, url } = getBlogPayload();
-      await page.getByTestId("blogForm");
+
       await page.getByTestId("showVisibilityButton").click();
+
+      await expect(page.getByTestId("blogForm")).toBeVisible();
       await page.getByTestId("blogFormTitleField").fill(title);
       await page.getByTestId("blogFormUrlField").fill(url);
       await page.getByTestId("postBlogButton").click();
-      await page.getByTestId("successMessageDisplay");
-      await page.getByText(/{title}/i);
-      await page.getByText(/{url}/i);
+
+      await expect(page.getByTestId("successMessageDisplay")).toBeVisible();
+      await expect(page.getByTestId("blogList")).not.toBeEmpty();
+      await expect(page.getByTestId("blogForm")).toBeHidden();
     });
   });
 });
