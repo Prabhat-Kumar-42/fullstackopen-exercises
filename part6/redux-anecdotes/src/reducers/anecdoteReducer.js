707e5fce209501cb48fefffe0321905babc9ac05
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-01 13:47:38 +0530

    added functionallity to sort displaye anecdotes by votes. Removed redudant console.logs

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,8 +20,6 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject);
 
 const reducer = (state = initialState, action) => {
-  console.log("state now: ", state);
-  console.log("action", action);
   const payload = action.payload;
   switch (action.type) {
     case "UPVOTE":
