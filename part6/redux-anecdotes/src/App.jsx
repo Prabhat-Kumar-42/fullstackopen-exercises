3695ed6eca5ef64ef406c5c87492dd00d9a44647
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-01 13:40:58 +0530

    added functionality to upvote and add new anecdote, exercise 6.3 and 6.4 done

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,34 +1,45 @@
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
+import { createAnecdote, upvote } from "./reducers/anecdoteReducer";
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
+  const anecdotes = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   const vote = (id) => {
-    console.log('vote', id)
-  }
+    console.log("vote", id);
+    dispatch(upvote(id));
+  };
+
+  const addAnecdote = (event) => {
+    event.preventDefault();
+    const contentTarget = event.target.content;
+    const action = createAnecdote(contentTarget.value);
+    dispatch(action);
+    contentTarget.value = "";
+  };
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => vote(anecdote.id)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="content" />
+        </div>
         <button>create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
