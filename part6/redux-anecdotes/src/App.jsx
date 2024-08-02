eaa92ff2c674189a88d5717254909e6c03a9d122
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-02 11:04:48 +0530

    updated to get anecdotes data from json-server

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import AnecdoteForm from "./components/AncedoteForm";
 import AnecdoteList from "./components/AnecdoteList";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
+import { setAnecdotes } from "./redux/reducers/anecdoteReducer";
+import anecdotesServices from "./services/anecdotes.services";
+import { useDispatch } from "react-redux";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdotesServices.getAnecdotes().then((responseData) => {
+      dispatch(setAnecdotes(responseData));
+    });
+  });
+
   return (
     <div>
       <h2>Anecdotes</h2>
