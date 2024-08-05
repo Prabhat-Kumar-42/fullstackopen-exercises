4413d2b3caf3c13978e7e3e686cf04b98ae8cc68
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-05 20:46:26 +0530

    updated notification component

--- part6/query-anecdotes/src/components/Notification.jsx
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -1,18 +1,47 @@
-const Notification = () => {
+import { useReducer } from "react";
+import notificationReducer, {
+  clearNotificationActionCreator,
+  displayNotificationActionCreator,
+} from "../reducers/notificationReducer";
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { useNotification } from "../contexts/notificationContext";
+
+const Notification = ({ message, timeToLive }) => {
   const style = {
-    border: 'solid',
+    border: "solid",
     padding: 10,
     borderWidth: 1,
-    marginBottom: 5
-  }
-  
-  if (true) return null
+    marginBottom: 5,
+  };
 
-  return (
-    <div style={style}>
-      
-    </div>
-  )
-}
+  const [notification, notificationDispatch] = useNotification();
+  const [timeoutRef, setTimeoutRef] = useState(null);
 
-export default Notification
+  const handleTimeout = () => {
+    if (timeoutRef) clearTimeout(timeoutRef);
+    const newTimeout = setTimeout(() => {
+      const clearAction = clearNotificationActionCreator();
+      notificationDispatch(clearAction);
+    }, timeToLive);
+    setTimeoutRef(newTimeout);
+  };
+
+  const handleNotification = () => {
+    const displayAction = displayNotificationActionCreator(message);
+    notificationDispatch(displayAction);
+  };
+
+  handleTimeout();
+  handleNotification();
+
+  if (!notification) return null;
+  return <div style={style}>{notification}</div>;
+};
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+  timeToLive: PropTypes.number.isRequired,
+};
+
+export default Notification;
