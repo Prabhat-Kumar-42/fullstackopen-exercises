106117242524224bcd17d188860915fdb91f9f73
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-07 14:19:25 +0530

    util module containing all urls

---
+++ part7/routed-anecdotes/src/utils/urls.js
@@ -0,0 +1,15 @@
+const home = "/";
+const anecdoteForm = "/create";
+const about = "/about";
+const anecdotes = "/anecdotes/";
+const specificAnecdote = "/anecdotes/:id";
+
+const anecdoteUrls = {
+  home,
+  about,
+  anecdotes,
+  anecdoteForm,
+  specificAnecdote,
+};
+
+export default anecdoteUrls;
