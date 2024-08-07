611c5c85e6052515c1b106d0aa4122471e985c97
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-07 14:21:10 +0530

    added link to specific anecdote view

--- part7/routed-anecdotes/src/components/AnecdoteList/AnecdoteList.jsx
+++ part7/routed-anecdotes/src/components/AnecdoteList/AnecdoteList.jsx
@@ -1,12 +1,21 @@
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
-      ))}
-    </ul>
-  </div>
-);
+import { Link } from "react-router-dom";
+import anecdoteUrls from "../../utils/urls";
+
+const AnecdoteList = ({ anecdotes }) => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map((anecdote) => (
+          <li key={anecdote.id}>
+            <Link to={anecdoteUrls.anecdotes + anecdote.id}>
+              {anecdote.content}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default AnecdoteList;
