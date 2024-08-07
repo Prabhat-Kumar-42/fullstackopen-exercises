d40cf1321aca7a1fc77f28ac2b422f94d8feef8d
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-07 14:52:41 +0530

    added notification handler and addition state for notification time management and passed them as props

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -3,20 +3,25 @@ import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 import anecdotesMockData from "./mockData/anecdotesMockData";
 import AnecdotesRoutes from "./components/AnecdotesRoutes/AnecdotesRoutes";
+import { useNavigate } from "react-router-dom";
+import Notification from "./components/Notification/Notification";
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState(anecdotesMockData);
 
   const [notification, setNotification] = useState("");
+  const [notificationTimeout, setNotificationTimeout] = useState(null);
+  const navigate = useNavigate();
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
     setAnecdotes(anecdotes.concat(anecdote));
+    navigate("/");
   };
 
   const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
 
-  const vote = (id) => {
+  const handleVotes = (id) => {
     const anecdote = anecdoteById(id);
 
     const voted = {
@@ -27,11 +32,24 @@ const App = () => {
     setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
   };
 
+  const handleNotification = (newNotification) => {
+    if (notificationTimeout) setNotificationTimeout(null);
+    const newNotificationTimeout = setTimeout(() => setNotification(""), 5000);
+    setNotification(newNotification);
+    setNotificationTimeout(newNotificationTimeout);
+  };
+
   return (
     <div>
       <h1>Software anecdotes</h1>
       <Menu />
-      <AnecdotesRoutes anecdotes={anecdotes} addNew={addNew} />
+      <Notification notification={notification} />
+      <AnecdotesRoutes
+        anecdotes={anecdotes}
+        addNew={addNew}
+        handleNotification={handleNotification}
+        handleVotes={handleVotes}
+      />
       <Footer />
     </div>
   );
