83cccca774ca7819c346c6ca398502da6f8749b3
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-07 11:50:46 +0530

    refactored mock data from app

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -4,24 +4,10 @@ import AnecdoteList from "./components/AnecdoteList/AnecdoteList";
 import Footer from "./components/Footer/Footer";
 import Menu from "./components/Menu/Menu";
 import AnecdoteForm from "./components/AnecdoteForm/AnecdoteForm";
+import anecdotesMockData from "./mockData/anecdotesMockData";
 
 const App = () => {
-  const [anecdotes, setAnecdotes] = useState([
-    {
-      content: "If it hurts, do it more often",
-      author: "Jez Humble",
-      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
-      votes: 0,
-      id: 1,
-    },
-    {
-      content: "Premature optimization is the root of all evil",
-      author: "Donald Knuth",
-      info: "http://wiki.c2.com/?PrematureOptimization",
-      votes: 0,
-      id: 2,
-    },
-  ]);
+  const [anecdotes, setAnecdotes] = useState(anecdotesMockData);
 
   const [notification, setNotification] = useState("");
 
