55cff7eb25fddee2a51b55e8e2ba88d967f05afd
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-07 14:19:48 +0530

    view for specific anecdote

---
+++ part7/routed-anecdotes/src/components/SpecificAnecdote/SpecificAnecdote.jsx
@@ -0,0 +1,14 @@
+const SpecificAnecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <div> has {anecdote.votes} votes</div>
+      <div>
+        for more info see <a href={anecdote.info}>{anecdote.info}</a>
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default SpecificAnecdote;
