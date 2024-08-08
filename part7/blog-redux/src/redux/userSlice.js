e8d9bd20929b7f752b42733c1b23364569c80893
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-09 01:44:22 +0530

    added userSlice

---
+++ part7/blog-redux/src/redux/userSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import userAsyncThunks from "./user/userAsyncThunk";
+import CONSTS from "../utils/config.util";
+
+const initialState = {
+  user: null,
+  authKey: null,
+  error: null,
+  status: null,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    clearUser: () => {
+      return null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(userAsyncThunks.login.pending, (state) => {
+        state.status = CONSTS.asyncThunkStatus.LOADING;
+        state.error = null;
+      })
+      .addCase(userAsyncThunks.login.fulfilled, (state, action) => {
+        state.status = CONSTS.asyncThunkStatus.SUCCEEDED;
+        state.user = action.payload.user;
+        state.authKey = action.payload.authToken;
+        state.error = null;
+      })
+      .addCase(userAsyncThunks.login.rejected, (state, action) => {
+        state.status = CONSTS.asyncThunkStatus.FAILED;
+        state.error = action.payload;
+      })
+      .addCase(userAsyncThunks.signup.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+      })
+      .addCase(userAsyncThunks.signup.fulfilled, (state) => {
+        state.status = "succeeded";
+        state.error = null;
+      })
+      .addCase(userAsyncThunks.signup.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.payload;
+      });
+  },
+});
+
+export default userSlice.reducer;
+export const { setUser, clearUser } = userSlice.actions;
