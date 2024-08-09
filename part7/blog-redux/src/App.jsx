22ebbe8cfe6fa946d577c84af21900a1148d3311
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-09 16:01:26 +0530

    added useEffect to set user from local storage if it exists, helps presist login when page refreshs and user state reset

--- part7/blog-redux/src/App.jsx
+++ part7/blog-redux/src/App.jsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
 import AppRoutes from "./routes/AppRoutes";
+import { useDispatch } from "react-redux";
+import { userAction } from "./redux/user/userSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) return;
+    const parsedUser = JSON.parse(user);
+    dispatch(
+      userAction.setUser({
+        user: parsedUser,
+      }),
+    );
+  }, []);
+
   return (
     <div>
       <AppRoutes />
