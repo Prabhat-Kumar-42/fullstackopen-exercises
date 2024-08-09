3deeca22768236a267d1deeec6673bd0cbb38815
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-09 20:00:40 +0530

    aadded blog services

---
+++ part7/blog-redux/src/redux/blog/blogServices.js
@@ -0,0 +1,17 @@
+import useResources from "../../hooks/useResources";
+import CONSTS from "../../utils/config.util";
+
+const baseUrl = CONSTS.serverUrls.blog.baseUrl;
+
+const blogService = (() => {
+  const { create, update, remove, resources } = useResources(baseUrl);
+
+  return {
+    createBlog: (payload) => create(payload),
+    updateBlog: (blog) => update(blog),
+    deleteBlog: (blog) => remove(blog),
+    getAllBlogs: () => resources(),
+  };
+})();
+
+export default blogService;
