c46e11109543e39a167c18a74700fea92c761204
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-09 17:30:18 +0530

    updated home view to use toggleable blog form component

--- part7/blog-redux/src/views/Home.jsx
+++ part7/blog-redux/src/views/Home.jsx
@@ -1,9 +1,17 @@
+import BlogForm from "../components/BlogForm/BlogForm";
+import Toggleable from "../components/Toggleable/Toggleable";
 import useUser from "../hooks/useUser";
 
 const Home = () => {
   const { user } = useUser();
+  const formDisplayText = "create new blog";
+  const formHideText = "cancle";
   return (
     <div>
+      <h1>blogs</h1>
+      <Toggleable toDisplayTitle={formDisplayText} toHideTitle={formHideText}>
+        <BlogForm />
+      </Toggleable>
       <span>{user.name} is logged in !! </span>
     </div>
   );
