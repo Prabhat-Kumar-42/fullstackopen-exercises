dca0fd5d29576c3d3ac70873ac2a7871aef04cd7
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-10 12:54:35 +0530

    added blogs to store

--- part7/blog-redux/src/redux/store.js
+++ part7/blog-redux/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
+import blogReducer from "./blog/blogSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    blogs: blogReducer,
   },
 });
 
