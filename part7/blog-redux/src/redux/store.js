85b61ef521f2a0b917931ab2c770f01c84172bb6
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-10 15:55:00 +0530

    added togglables reference list to store.

--- part7/blog-redux/src/redux/store.js
+++ part7/blog-redux/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice";
 import blogReducer from "./blog/blogSlice";
+import toggleablesReducer from "./toggleableComponents/toggleablesSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     blogs: blogReducer,
+    toggleables: toggleablesReducer,
   },
 });
 
