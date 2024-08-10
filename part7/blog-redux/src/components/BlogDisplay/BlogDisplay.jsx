95be2c836ea4ced788e0e8fc0e5d43d0f49256de
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-10 13:30:01 +0530

    updated BlogDisplay to use BlogListDisplay component

--- part7/blog-redux/src/components/BlogDisplay/BlogDisplay.jsx
+++ part7/blog-redux/src/components/BlogDisplay/BlogDisplay.jsx
@@ -1,16 +1,18 @@
-import useBlog from "../../hooks/useBlog";
 import BlogForm from "../BlogForm/BlogForm";
 import Toggleable from "../Toggleable/Toggleable";
+import BlogListDisplay from "./BlogsListDisplay/BlogsListDisplay";
 
 const BlogDisplay = () => {
-  const { blogs } = useBlog();
   const formDisplayText = "create new blog";
   const formHideText = "cancle";
 
   return (
-    <Toggleable toDisplayTitle={formDisplayText} toHideTitle={formHideText}>
-      <BlogForm />
-    </Toggleable>
+    <div>
+      <Toggleable toDisplayTitle={formDisplayText} toHideTitle={formHideText}>
+        <BlogForm />
+      </Toggleable>
+      <BlogListDisplay />
+    </div>
   );
 };
 
