f71a8091253fdb84619a98b378926f6e5d632759
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-10 12:27:16 +0530

    implemented logout handler

--- part7/blog-redux/src/views/Home.jsx
+++ part7/blog-redux/src/views/Home.jsx
@@ -1,18 +1,34 @@
+import { useDispatch } from "react-redux";
 import BlogForm from "../components/BlogForm/BlogForm";
+import Button from "../components/Button/Button";
 import Toggleable from "../components/Toggleable/Toggleable";
 import useUser from "../hooks/useUser";
+import userAsyncThunks from "../redux/user/userAsyncThunk";
+import { useNavigate } from "react-router-dom";
+import CONSTS from "../utils/config.util";
 
 const Home = () => {
   const { user } = useUser();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const formDisplayText = "create new blog";
   const formHideText = "cancle";
+  const logoutText = "logout";
+
+  const handleLogout = async () => {
+    const loginUrl = CONSTS.clientUrls.login;
+    dispatch(userAsyncThunks.logout());
+    navigate(loginUrl);
+  };
+
   return (
     <div>
       <h1>blogs</h1>
+      <span>{user.name} is logged in !! </span>
+      <Button text={logoutText} onClick={handleLogout} />
       <Toggleable toDisplayTitle={formDisplayText} toHideTitle={formHideText}>
         <BlogForm />
       </Toggleable>
-      <span>{user.name} is logged in !! </span>
     </div>
   );
 };
