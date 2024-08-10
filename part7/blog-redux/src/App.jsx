215617d468c02138944b5fd8575b75c44737df1e
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-10 11:37:08 +0530

    initialized blogs

--- part7/blog-redux/src/App.jsx
+++ part7/blog-redux/src/App.jsx
@@ -1,12 +1,23 @@
 import AppRoutes from "./routes/AppRoutes";
 import usePresistedUser from "./hooks/usePresistedUser";
+import { useDispatch } from "react-redux";
+import blogAsyncThunks from "./redux/blog/blogAsyncThunks";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
   usePresistedUser();
+
+  useEffect(() => {
+    // initiaize blogs
+    dispatch(blogAsyncThunks.getAllBlogs());
+  }, [dispatch]);
+
   return (
     <div>
       <AppRoutes />
     </div>
   );
 }
+
 export default App;
