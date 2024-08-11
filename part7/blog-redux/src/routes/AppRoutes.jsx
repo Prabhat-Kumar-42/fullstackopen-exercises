b2b17f7980cbc37bd6152dd0e837c73b6828c2bc
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-11 20:55:00 +0530

    added routes for userlist page and specific user page, update home route to blog route.

--- part7/blog-redux/src/routes/AppRoutes.jsx
+++ part7/blog-redux/src/routes/AppRoutes.jsx
@@ -3,7 +3,9 @@ import CONSTS from "../utils/config.util";
 import LoginForm from "../components/LoginSignUpComponents/LoginForm/LoginForm";
 import SignUpForm from "../components/LoginSignUpComponents/SignUpForm/SignUpForm";
 import useUser from "../hooks/useUser";
-import Home from "../views/Home";
+import UserList from "../components/Users/UserList/UserList";
+import SpecificUserPage from "../components/Users/SpecificUserPage/SpecificUserPage";
+import BlogDisplay from "../components/BlogDisplay/BlogDisplay";
 
 const AppRoutes = () => {
   const { user } = useUser();
@@ -13,18 +15,36 @@ const AppRoutes = () => {
       <Route
         path={CONSTS.clientUrls.login}
         element={
-          !user ? <LoginForm /> : <Navigate to={CONSTS.clientUrls.home} />
+          !user ? <LoginForm /> : <Navigate to={CONSTS.clientUrls.blogs} />
         }
       />
       <Route
         path={CONSTS.clientUrls.signup}
         element={
-          !user ? <SignUpForm /> : <Navigate to={CONSTS.clientUrls.home} />
+          !user ? <SignUpForm /> : <Navigate to={CONSTS.clientUrls.blogs} />
         }
       />
       <Route
-        path={CONSTS.clientUrls.home}
-        element={user ? <Home /> : <Navigate to={CONSTS.clientUrls.login} />}
+        path={CONSTS.clientUrls.blogs}
+        element={
+          user ? <BlogDisplay /> : <Navigate to={CONSTS.clientUrls.login} />
+        }
+      />
+      <Route
+        path={CONSTS.clientUrls.users}
+        element={
+          user ? <UserList /> : <Navigate to={CONSTS.clientUrls.login} />
+        }
+      />
+      <Route
+        path={CONSTS.clientUrls.specificUser}
+        element={
+          user ? (
+            <SpecificUserPage />
+          ) : (
+            <Navigate to={CONSTS.clientUrls.login} />
+          )
+        }
       />
     </Routes>
   );
