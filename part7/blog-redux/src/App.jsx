5d705bd5b85ef862bc13d4c3dba4650eb1ee67b6
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-11 20:55:30 +0530

    updated to initialize usersList and show page header

--- part7/blog-redux/src/App.jsx
+++ part7/blog-redux/src/App.jsx
@@ -5,20 +5,26 @@ import blogAsyncThunks from "./redux/blog/blogAsyncThunks";
 import { useEffect } from "react";
 import ErrorMessageDisplay from "./components/Notifications/ErrorMessageDisplay/ErrorMessageDisplay";
 import SuccessMessageDisplay from "./components/Notifications/SuccessMessageDisplay/SuccessMessageDisplay";
+import userAsyncThunks from "./redux/user/userAsyncThunk";
+import PageHeader from "./components/PageHeader/PageHeader";
+import useUser from "./hooks/useUser";
 
 function App() {
   const dispatch = useDispatch();
+  const { user } = useUser();
   usePresistedUser();
 
   useEffect(() => {
     // initiaize blogs
     dispatch(blogAsyncThunks.getAllBlogs());
+    dispatch(userAsyncThunks.getAllUser());
   }, [dispatch]);
 
   return (
     <div>
       <ErrorMessageDisplay />
       <SuccessMessageDisplay />
+      {user && <PageHeader />}
       <AppRoutes />
     </div>
   );
