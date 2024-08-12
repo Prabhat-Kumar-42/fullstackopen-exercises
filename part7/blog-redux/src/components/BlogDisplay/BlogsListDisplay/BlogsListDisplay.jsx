3741acfab9e4e90008a2c759b38f8d82249e6327
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-12 10:24:25 +0530

    updated: instead of blog details, now displays blog title that links to respective specific blog view

--- part7/blog-redux/src/components/BlogDisplay/BlogsListDisplay/BlogsListDisplay.jsx
+++ part7/blog-redux/src/components/BlogDisplay/BlogsListDisplay/BlogsListDisplay.jsx
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
 import useBlog from "../../../hooks/useBlog";
-import SpecificBlogDisplay from "../SpecificBlogDisplay/SpecificBlogDisplay";
-
+import CONSTS from "../../../utils/config.util";
+import "./blogStyle.css";
 const BlogListDisplay = () => {
   const { blogs } = useBlog();
   const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
-
+  const baseUrl = CONSTS.clientUrls.blogs;
   return (
-    <div>
+    <div className="blogStyle">
       {sortedBlogs.map((blog) => (
-        <SpecificBlogDisplay blog={blog} key={blog.id} />
+        <div key={blog.id}>
+          <Link to={`${baseUrl}/${blog.id}`}>{blog.title}</Link>
+        </div>
       ))}
     </div>
   );
