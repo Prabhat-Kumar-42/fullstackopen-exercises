c933b2fcef5284147e0503f232bc7d7085bb5c11
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-12 13:02:38 +0530

    update: [added condition if there's no one commented to the blog yet]

--- part7/blog-redux/src/components/Comments/CommentDisplay/BlogCommentDisplay.jsx
+++ part7/blog-redux/src/components/Comments/CommentDisplay/BlogCommentDisplay.jsx
@@ -1,12 +1,13 @@
 import BlogCommentForm from "../CommentForm/BlogCommentForm";
 
 const BlogCommentDisplay = ({ blog }) => {
-  if (!blog.comments.length) return <div>Be first to comment !!</div>;
   const baseKey = `comment-${blog.id}-`;
+  const noComment = blog.comments.length === 0;
   return (
     <div>
       <h3>Comments</h3>
       <BlogCommentForm blog={blog} />
+      {noComment && <span>Be The First To Comment !!</span>}
       <ul>
         {blog.comments.map((comment, index) => (
           <li key={baseKey + index}>{comment}</li>
