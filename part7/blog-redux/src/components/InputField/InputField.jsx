f5bb83b5071131c74fb079bd5ea5084bec1e904b
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-13 18:47:50 +0530

    styled input field

--- part7/blog-redux/src/components/InputField/InputField.jsx
+++ part7/blog-redux/src/components/InputField/InputField.jsx
@@ -1,10 +1,16 @@
 const InputField = ({ props }) => {
-  const { name } = props;
-  const filterAttributes = ({ clearField, ...rest }) => rest;
+  const { name, className, ...rest } = props; // Extract className and other props
+
   return (
-    <div>
-      <label htmlFor={name}>{name}: </label>
-      <input {...filterAttributes(props)} />
+    <div className="flex flex-col space-y-2">
+      <label htmlFor={name} className="text-gray-700 font-medium">
+        {name}
+      </label>
+      <input
+        id={name}
+        {...rest}
+        className={`border border-gray-300 rounded-lg px-4 py-2 text-gray-700 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-colors duration-200 ease-in-out ${className}`}
+      />
     </div>
   );
 };
