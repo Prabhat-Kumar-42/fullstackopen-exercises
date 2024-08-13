5eb4d26a6cb9bfbe7c7e18ba06a2bf47665c400c
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-13 18:41:12 +0530

    styled bloglist

--- part7/blog-redux/src/components/BlogDisplay/BlogsListDisplay/BlogsListDisplay.jsx
+++ part7/blog-redux/src/components/BlogDisplay/BlogsListDisplay/BlogsListDisplay.jsx
@@ -1,17 +1,35 @@
 import { Link } from "react-router-dom";
 import useBlog from "../../../hooks/useBlog";
 import CONSTS from "../../../utils/config.util";
-import "./blogStyle.css";
+
 const BlogListDisplay = () => {
   const { blogs } = useBlog();
-  if (!blogs.length) return <div>No Blogs to display</div>;
+
+  if (!blogs.length) {
+    return (
+      <div className="text-center text-gray-500 mt-6">
+        <p>No Blogs to display</p>
+      </div>
+    );
+  }
+
   const sortedBlogs = [...blogs].sort((a, b) => b.likes - a.likes);
   const baseUrl = CONSTS.clientUrls.blogs;
+
   return (
-    <div>
+    <div className="space-y-4 mt-6">
       {sortedBlogs.map((blog) => (
-        <div className="blogStyle" key={blog.id}>
-          <Link to={`${baseUrl}/${blog.id}`}>{blog.title}</Link>
+        <div
+          key={blog.id}
+          className="p-4 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+        >
+          <Link
+            to={`${baseUrl}/${blog.id}`}
+            className="text-xl font-semibold text-blue-600 hover:text-blue-800"
+          >
+            {blog.title}
+          </Link>
+          <p className="text-sm text-gray-600 mt-1">Likes: {blog.likes}</p>
         </div>
       ))}
     </div>
