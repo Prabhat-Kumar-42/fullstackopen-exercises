b0509a932a6ee0cea72f87adf6e0c30ceaf35c5d
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-13 19:06:13 +0530

    styled blogCommentDisplay

--- part7/blog-redux/src/components/Comments/CommentDisplay/BlogCommentDisplay.jsx
+++ part7/blog-redux/src/components/Comments/CommentDisplay/BlogCommentDisplay.jsx
@@ -3,14 +3,24 @@ import BlogCommentForm from "../CommentForm/BlogCommentForm";
 const BlogCommentDisplay = ({ blog }) => {
   const baseKey = `comment-${blog.id}-`;
   const noComment = blog.comments.length === 0;
+
   return (
-    <div>
-      <h3>Comments</h3>
+    <div className="p-6 mt-16 bg-white rounded-lg ">
+      <h3 className="text-xl font-semibold text-gray-800 mb-4">Comments</h3>
       <BlogCommentForm blog={blog} />
-      {noComment && <span>Be The First To Comment !!</span>}
-      <ul>
+      {noComment && (
+        <span className="block text-gray-500 mt-4">
+          Be the first to comment!
+        </span>
+      )}
+      <ul className="mt-4 space-y-2">
         {blog.comments.map((comment, index) => (
-          <li key={baseKey + index}>{comment}</li>
+          <li
+            key={baseKey + index}
+            className="p-4 bg-gray-100 rounded-lg shadow-sm"
+          >
+            {comment}
+          </li>
         ))}
       </ul>
     </div>
