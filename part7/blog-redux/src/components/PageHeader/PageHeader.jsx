6e8396f65e9ec70defc7f5494096621a773d0180
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-13 18:26:53 +0530

    styled PageHeader

--- part7/blog-redux/src/components/PageHeader/PageHeader.jsx
+++ part7/blog-redux/src/components/PageHeader/PageHeader.jsx
@@ -2,9 +2,13 @@ import Navbar from "../Navbar/Navbar";
 
 const PageHeader = () => {
   return (
-    <div>
+    <div className="bg-gray-100 shadow-md">
       <Navbar />
-      <h1>blogs app</h1>
+      <div className="max-w-6xl mx-auto px-8 py-6">
+        <h1 className="text-4xl font-bold text-gray-800 text-center">
+          Blogs App
+        </h1>
+      </div>
     </div>
   );
 };
