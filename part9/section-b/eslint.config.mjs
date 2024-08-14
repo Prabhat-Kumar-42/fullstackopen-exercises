c67e44bd8314b261ab6584eea9db0a867204cf71
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-14 19:03:09 +0530

    changed sourceType to module

--- part9/section-b/eslint.config.mjs
+++ part9/section-b/eslint.config.mjs
@@ -2,11 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
+
