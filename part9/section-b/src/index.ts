cb04c1e7b26b400783fd14b26399aa51f29cc055
Prabhat-Kumar-42 <prabhatkumarverma42@gmail.com>
2024-08-25 22:32:33 +0530

    added exercise-endpoint to app

--- part9/section-b/src/index.ts
+++ part9/section-b/src/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 import "express-async-errors";
 import { ErrorHandler } from "./middlewares/ErrorHandler";
 import bmiRouter from "./routes/bmiRoute";
+import exerciseRouter from "./routes/exerciseRoute";
 
 const app = express();
 app.use(express.json());
 
 app.use("/bmi", bmiRouter);
+app.use("/exercises", exerciseRouter);
 
 app.use(ErrorHandler);
 
